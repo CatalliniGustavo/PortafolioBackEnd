affcb55fcc7daf93c1b5ade7313ca3c5ea2104b8
CatalliniGustavo <catallinigustavo@gmail.com>
2022-07-24 15:18:03 -0300

    p

--- src/main/java/com/catallinigustavo/pgc/Security/Controller/AuthController.java
+++ src/main/java/com/catallinigustavo/pgc/Security/Controller/AuthController.java
@@ -48,7 +48,7 @@ public class AuthController {
         
         if (nuevoUsuario.getPassword().length() != 8 )
             return new ResponseEntity(new Mensaje("La contraseña tiene que tener 8 caracteres"), HttpStatus.BAD_REQUEST);
-        if (nuevoUsuario.getApellido().length() == 0 || nuevoUsuario.getNombre().length() == 0)
+        if (nuevoUsuario.getApellido() == null || nuevoUsuario.getNombre() == null)
             return new ResponseEntity(new Mensaje("Falta nombre 0/y apellido"), HttpStatus.BAD_REQUEST);
         if (usuarioService.existsByEmail(nuevoUsuario.getEmail()))
             return new ResponseEntity(new Mensaje("Ese Email ya esta relacionado a otra cuenta"), HttpStatus.BAD_REQUEST);
